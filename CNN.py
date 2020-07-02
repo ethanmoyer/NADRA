fe9170694d886397b45b93e4295f10473e1d4029
ethan <57536747+ethanmoyer@users.noreply.github.com>
2020-07-02 13:49:55 -0400

    cleaned

--- CNN.py
+++ CNN.py
@@ -10,25 +10,24 @@ from sklearn.metrics import classification_report, confusion_matrix
 import tensorflow as tf
 from tensorflow.keras import regularizers
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv1D, MaxPooling1D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D, BatchNormalization, ReLU
 
 # Retrieves both the post processed training data, the post processed test data, and the post processed validation data.
-taining_data_loc = str(input('Enter the location of the processed training data: '))
+taining_data_loc = 'processed_data/train_data.csv'#str(input('Enter the location of the processed training data: '))
 if (taining_data_loc.find("processed_data") < 0 or taining_data_loc.find("train") < 0):
 	print("Please enter the location of a training data set from the processed_sdata directory.")
 	exit()
 dataset_train = pd.read_csv(taining_data_loc)
 
-test_data_loc = str(input('Enter the location of the processed test data: '))
+test_data_loc = 'processed_data/test_data.csv'#str(input('Enter the location of the processed test data: '))
 if (test_data_loc.find("processed_data") < 0 or test_data_loc.find("test") < 0):
 	print("Please enter the location of a test data set from the processed_data directory.")
 	exit()
 dataset_test = pd.read_csv(test_data_loc)
 
-dataset_validation_loc = str(input('Enter the location of the processed validation data: '))
-if (dataset_validation_loc.find("processed_data") < 0 or dataset_validation_loc.find("train") < 0):
-	print("Please enter the location of a training data set from the processed_data directory.")
+dataset_validation_loc = 'processed_data/val_data.csv'#str(input('Enter the location of the processed validation data: '))
+if (dataset_validation_loc.find("processed_data") < 0 or dataset_validation_loc.find("val") < 0):
+	print("Please enter the location of a validation data set from the processed_data directory.")
 	exit()
 dataset_validation = pd.read_csv(dataset_validation_loc)
 
@@ -66,18 +65,39 @@ y_val = dataset_y_numpy.reshape(y_val.shape[0], 1, 1)
 
 # Initializes the main variables for the model.
 batch_size = dataset_train.shape[0]
-epochs = 30
+epochs = 50
 num_classes = 2
 
 # Builds the model
+# Make the model deeper and narrower
 model = Sequential()
 model.add(Conv1D(filters=64, kernel_size=3,activation='softplus', input_shape=(1, X_train.shape[2]),kernel_regularizer=regularizers.l2(0.01), activity_regularizer=regularizers.l1(0.01), padding='same',strides=3))
-model.add(Conv1D(filters=64, kernel_size=3, activation='linear',
-			padding='same', strides=3))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(BatchNormalization())
 model.add(Dropout(0.5))
-model.add(MaxPooling1D(pool_size=2,padding='same'))
+
+model.add(Conv1D(filters=128, kernel_size=3, activation='linear',
+			padding='same', strides=3))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(BatchNormalization())
+
+model.add(Conv1D(filters=256, kernel_size=3, activation='linear',
+			padding='same', strides=3))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(BatchNormalization())
+
+model.add(Conv1D(filters=512, kernel_size=3, activation='linear',
+			padding='same', strides=3))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(BatchNormalization())
+
+model.add(Conv1D(filters=1024, kernel_size=3, activation='linear',
+			padding='same', strides=3))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(BatchNormalization())
+
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))            
+model.add(Dense(1024, activation='relu'))            
 model.add(Dense(num_classes, activation='softmax'))
 
 # Compiles the model
@@ -88,9 +108,7 @@ model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy,
 model.summary()
 
 # Cross validates with validation data.
-model_train = model.fit(X_train, y_train, batch_size=batch_size,
-                        epochs=epochs,verbose=1,
-                        validation_data=(dataset_validation, y_val))
+model_train = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,verbose=1, validation_data=(dataset_validation, y_val))
 
 # Predicts using the model on a new data set.
 y_pred = model.predict_classes(X_test)
